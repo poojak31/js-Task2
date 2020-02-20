1856586e6d746a40b47db9b66f6cae3de1a24bf1
poojak31 <pooja.kolape@qliktag.com>
2020-02-20 15:33:31 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 // Import stylesheets
 //import './style.css';
 
-function EvenOdd()
-{
+// function EvenOdd()
+// {
  
-       var no1=document.getElementById('no').value;
+//        var no1=document.getElementById('no').value;
 
-       if(no1%2==0)
+//        if(no1%2==0)
+//        {
+//           document.getElementById('result').innerHTML=`The number ${no1} is even.`;
+//        }
+//        else
+//        {
+//           document.getElementById('result').innerHTML=`The number ${no1} is odd.`
+//        }
+// }
+
+// const butts=document.querySelector('#button');
+
+// butts.addEventListener('click',EvenOdd);
+
+function even() 
+{
+   var no1=document.getElementById('no').value;
+
+  let odd=evenodd(no1);
+
+   console.log(odd);
+  
+}
+
+function evenodd(no1)
+{
+     if(no1%2==0)
        {
-          document.getElementById('result').innerHTML=`The number ${no1} is even.`;
+        return  document.getElementById('result').innerHTML=`The number ${no1} is even.`;
        }
        else
        {
-          document.getElementById('result').innerHTML=`The number ${no1} is odd.`
+         return document.getElementById('result').innerHTML=`The number ${no1} is odd.`;
        }
 }
 
 const butts=document.querySelector('#button');
 
-butts.addEventListener('click',EvenOdd);
+ butts.addEventListener('click',even);
+
 
